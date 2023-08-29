3c69487651c3a9d01349d64ce5120ee4a6e89367
Anssi Peltola <anssipeltola@hotmail.com>
2023-08-29 11:12:07 +0300

    GoTopButton added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const contact = document.querySelector(".contact");
 const overlay = document.querySelector(".overlay");
 const btnCloseContact = document.querySelector(".close-contact");
 const btnOpenContact = document.querySelector(".show-contact");
+const mybutton = document.getElementById("goTopBtn");
 
 // Funktio joka ottaa contact classista hidden pois, jolloin Contact ikkuna aukeaa. On määritetty sitemmin CSS tiedostossa onko se hidden vai ei.
 const openContact = function () {
@@ -33,3 +34,25 @@ document.addEventListener("keydown", function (e) {
 });
 
 // https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648437#content
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 450 ||
+    document.documentElement.scrollTop > 450
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
